722fdc503c786fc9dcd8c372ece5ade3154db2bc
Noah Baron <32229274+noahbaron91@users.noreply.github.com>
2024-09-05 15:55:07 -0700

    feat: initial ws

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -75,6 +75,33 @@ export class WebSocketServer extends DurableObject {
 
 		return `Hello, ${counter}!`;
 	}
+
+	async fetch(request: Request): Promise<Response> {
+		// Creates two ends of a WebSocket connection.
+		const webSocketPair = new WebSocketPair();
+		const [client, server] = Object.values(webSocketPair);
+
+		// Calling `accept()` tells the runtime that this WebSocket is to begin terminating
+		// request within the Durable Object. It has the effect of "accepting" the connection,
+		// and allowing the WebSocket to send and receive messages.
+		server.accept();
+
+		// Upon receiving a message from the client, the server replies with the same message,
+		// and the total number of connections with the "[Durable Object]: " prefix
+		server.addEventListener('message', (event) => {
+			server.send(`[Durable Object] ${event.data}`);
+		});
+
+		// If the client closes the connection, the runtime will close the connection too.
+		server.addEventListener('close', (cls) => {
+			server.close(cls.code, 'Durable Object is closing WebSocket');
+		});
+
+		return new Response(null, {
+			status: 101,
+			webSocket: client,
+		});
+	}
 }
 
 export default {
@@ -87,18 +114,33 @@ export default {
 	 * @returns The response to be sent back to the client
 	 */
 	async fetch(request, env, ctx): Promise<Response> {
-		// We will create a `DurableObjectId` using the pathname from the Worker request
-		// This id refers to a unique instance of our 'MyDurableObject' class above
-		let id: DurableObjectId = env.WEBSOCKET_SERVER.idFromName(new URL(request.url).pathname);
+		if (request.url.endsWith('/websocket')) {
+			const upgradeHeader = request.headers.get('Upgrade');
 
-		// This stub creates a communication channel with the Durable Object instance
-		// The Durable Object constructor will be invoked upon the first call for a given id
-		let stub = env.WEBSOCKET_SERVER.get(id);
+			if (!upgradeHeader || upgradeHeader !== 'websocket') {
+				return new Response('Durable Object expected Upgrade: websocket', { status: 426 });
+			}
 
-		// We call the `sayHello()` RPC method on the stub to invoke the method on the remote
-		// Durable Object instance
-		let greeting = await stub.sayHello('world');
+			let id = env.WEBSOCKET_SERVER.idFromName('main');
+			let stub = env.WEBSOCKET_SERVER.get(id);
 
-		return new Response(greeting);
+			return stub.fetch(request);
+
+			// We will create a `DurableObjectId` using the pathname from the Worker request
+			// This id refers to a unique instance of our 'MyDurableObject' class above
+			// let id: DurableObjectId = env.WEBSOCKET_SERVER.idFromName(new URL(request.url).pathname);
+
+			// // This stub creates a communication channel with the Durable Object instance
+			// // The Durable Object constructor will be invoked upon the first call for a given id
+			// let stub = env.WEBSOCKET_SERVER.get(id);
+
+			// // We call the `sayHello()` RPC method on the stub to invoke the method on the remote
+			// // Durable Object instance
+			// let greeting = await stub.sayHello('world');
+
+			// return new Response(greeting);
+		}
+
+		return new Response('Not Found', { status: 404 });
 	},
 } satisfies ExportedHandler<Env>;
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,44 @@
+import { useEffect, useState } from 'react';
+
 function App() {
-  return <h1 className='text-3xl font-bold underline'>countinorder.com</h1>;
+  const [count, setCount] = useState(0);
+  const [newCount, setNewCount] = useState(0);
+
+  useEffect(() => {
+    const socket = new WebSocket('ws://localhost:8787/websocket');
+
+    // Connection opened
+    socket.addEventListener('open', (event) => {
+      console.log('Connected to server', event);
+      socket.send('Hello Server!');
+    });
+
+    // Listen for messages
+    socket.addEventListener('message', (event) => {
+      console.log('Message from server ', event.data);
+    });
+  }, []);
+
+  const handleSubmit = async () => {
+    console.log('handleSubmit', newCount);
+  };
+
+  return (
+    <div className='text-3xl'>
+      <p>Current count: {count}</p>
+      <div className='flex items-center gap-5'>
+        <input
+          type='number'
+          value={newCount}
+          onChange={(event) => setNewCount(Number(event.target.value))}
+        />
+        <button type='button' onClick={handleSubmit}>
+          Submit
+        </button>
+      </div>
+      countinorder.com
+    </div>
+  );
 }
 
 export default App;
