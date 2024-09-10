cf9439be52cfdf607b893e684b19287cda7bc10e
Noah Baron <32229274+noahbaron91@users.noreply.github.com>
2024-09-10 08:46:39 -0700

    feat: app-env

--- backend/src/db.ts
+++ backend/src/db.ts
@@ -1,6 +1,8 @@
 import { Client } from 'pg';
 import { config } from './config';
 
+console.log(process.env.APP_ENV);
+
 const db = new Client({
   connectionString: config.databaseURL,
   ...(process.env.APP_ENV === 'production' && {
