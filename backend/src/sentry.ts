c07557359073858519246deb859bfd8c874072ff
Noah Baron <32229274+noahbaron91@users.noreply.github.com>
2024-09-26 01:53:14 -0700

    feat: increase trace

--- backend/src/sentry.ts
+++ backend/src/sentry.ts
@@ -4,7 +4,7 @@ import { nodeProfilingIntegration } from '@sentry/profiling-node';
 Sentry.init({
   dsn: 'https://32ce60c9fd2f016f89a6768b31d3ee7b@o4506862653341696.ingest.us.sentry.io/4507986420826112',
   integrations: [nodeProfilingIntegration()],
-  tracesSampleRate: 0.01,
+  tracesSampleRate: 0.1,
   profilesSampleRate: 1.0,
   enabled: process.env.NODE_ENV === 'production',
 });
